2d4d4c27d4c09217cd1ae23ade35b76cab9a776a
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-02-07 20:35:33 +0300

    done

---
+++ python-power-mod-power.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def result(a,b,m):
+    print(pow(a,b))
+    print(pow(a,b,m))
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+result(a,b,m)
