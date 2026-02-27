c756c3a4807075adce9e77ad1c0b0bfb976dc242
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-02-27 18:32:48 +0300

    done using some maths and sliding window

---
+++ C_Number_of_Segments_with_Small_Sum.py
@@ -0,0 +1,14 @@
+n , s = (map(int,input().split()))
+arr = list(map(int,input().split()))
+ans , curr_sum  ,left  = 0 ,0 ,0
+for right in range(n):
+    curr_sum += arr[right]
+
+    while curr_sum > s:
+        curr_sum -= arr[left]
+        left += 1
+    
+    ans += (right-left + 1)
+
+print(ans)
+
