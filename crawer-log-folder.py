6fecf48212e1fde9ff4e272ed645923696fd5b90
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-03-27 18:49:02 +0300

    simple stack operation

---
+++ crawer-log-folder.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        for char in logs:
+            if char == '../':
+                if stack:
+                    stack.pop()
+            elif char == './':
+                continue
+            else:
+                stack.append(char)
+        return len(stack)
+
+        
